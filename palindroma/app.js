3f834019bcc4ded4095841c8233ef78419baa993
meta-ela <anxhelameta@live.it>
2022-05-26 15:29:11 +0200

    Create repo

---
+++ palindroma/app.js
@@ -0,0 +1,12 @@
+/*
+Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+
+- chiedere parola
+- 
+*/
+
+
+const askUser = prompt("Inserisci una parola.");
