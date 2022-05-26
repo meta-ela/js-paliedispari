9722ae366f1e6240a92c2125976baa68616a1afb
meta-ela <anxhelameta@live.it>
2022-05-26 16:51:43 +0200

    Adds function

--- palindroma/app.js
+++ palindroma/app.js
@@ -4,9 +4,25 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
 
-- chiedere parola
-- 
 */
 
+// chiedo all'utente una parola
+const wordUser = prompt("Inserisci una parola.");
 
-const askUser = prompt("Inserisci una parola.");
+console.log(wordUser);
+
+
+function isPalindrome(word) {
+    // la parola viene:
+    //                                           (0) (1) (2)        
+    // split: scomposta in stringhe separate --> "a" "b" "a"
+    // reverse: riposizionato al contrario  -->  (2) (1) (0) 
+    //
+    // joim: la parola viene ricomposta DOPO il reverse mantiene il nuovo riposizionamento
+    let reverseWordUser = word.split('').reverse().join('');
+
+    // se la parola in reverse è uguale alla parola inserita = polidromo
+    return reverseWordUser === wordUser;
+}
+
+console.log(isPalindrome(wordUser));
